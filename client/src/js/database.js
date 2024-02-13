45d90b1ed79aee3eabf4173875dab51cc02cf280
Trevor Christensen <trevorchristensen3405@gmail.com>
2024-02-13 09:58:18 -0700

    adding logic to database.js

---
+++ client/src/js/database.js
@@ -0,0 +1,35 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  const db = await initdb();
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  await store.clear();
+  await store.put({ content });
+  await tx.done;
+}
+
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  const db = await initdb();
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const data = await store.getAll();
+  await tx.done;
+  return data.length > 0 ? data[0].content : null;
+};
+
+initdb();
